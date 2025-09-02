edbbf922ed52334ca38bc09dd5e61d30426d290a
Edrianne Oreo <oreoedr@gmail.com>
2025-09-03 00:29:21 +0800

    Create Comments.java

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,19 @@
+public class Comments {
+    public static void main(String[] args) {
+
+        // This is a single-line comment
+        System.out.println("This line uses a single-line comment.");
+
+        /*
+         This is a multi-line comment
+         It can span across multiple lines
+        */
+        System.out.println("This line uses a multi-line comment.");
+
+        /**
+         * This is a documentation comment
+         * It can be used to describe classes or methods
+         */
+        System.out.println("This line uses a documentation comment.");
+    }
+}
