091a3400f84e4d9f84b20f78a5f7f971500b897f
Edrianne Oreo <oreoedr@gmail.com>
2025-09-03 00:06:35 +0800

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class PaboritongKanta {
+    public static void main(String[] args) {
+        System.out.println("Swagger so impressive and I don't need a necklace");
+        System.out.println("But these b****es get impressed when you pull up in that 7");
+        System.out.println("Them 6's, them Benzes, I gets get the freshest");
+        System.out.println("Raf Simons, Rick Owens usually what I'm dressed in");
+    }
+}
