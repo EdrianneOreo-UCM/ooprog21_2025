922c6144e6c81fec13373a5be2959dd29197ab45
Edrianne Oreo <oreoedr@gmail.com>
2025-09-03 06:05:25 -0700

    Add files via upload

---
+++ Chapter_1/TableAndChairs.java
@@ -0,0 +1,46 @@
+public class TableAndChairs {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("*                                         *");
+        }
+
+        for (int i = 1; i <= 8; i++) {
+            System.out.print("*");
+        }
+         
+            System.out.print("      ");
+
+        for (int i = 1; i <= 15; i++) {
+            System.out.print("*");
+        }
+            System.out.print("      ");
+
+         for (int i = 0; i < 8; i++) {
+            System.out.print("*");
+        }
+        
+            System.out.println("      ");
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("*      *      *             *      *      *");
+        }
+
+
+
+
+
+
+
+        
+
+        
+
+         
+
+
+    
+
+    }
+}
+        
