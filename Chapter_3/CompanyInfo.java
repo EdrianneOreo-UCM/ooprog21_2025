d181430b90d5d236302879d09a2e4fb0ff417212
Edrianne Oreo <oreoedr@gmail.com>
2025-09-15 08:56:40 +0800

    Create CompanyInfo.java

---
+++ Chapter_3/CompanyInfo.java
@@ -0,0 +1,14 @@
+public class CompanyInfo {
+
+    
+    public static void displayHours() {
+        System.out.println("Monday - Friday  9 AM - 5 PM");
+        System.out.println("Saturday         10 AM - 2 PM");
+        System.out.println("Sunday           Closed");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Smart Electronics");
+        displayHours();
+    }
+}
