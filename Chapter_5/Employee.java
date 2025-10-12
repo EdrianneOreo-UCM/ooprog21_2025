8a15ca7912fb336043edd3a4794aabceb35f4a2c
Edrianne Oreo <oreoedr@gmail.com>
2025-10-13 00:18:22 +0800

    Create Employee.java

---
+++ Chapter_5/Employee.java
@@ -0,0 +1,38 @@
+public class Employee {
+    private int employeeNumber;
+    private double payRate;
+    private static final int MAX_EMPLOYEE_NUMBER = 9999;
+    private static final double MAX_RATE = 60.00;
+    private static final double OVERTIME_MULTIPLIER = 1.5;
+
+    public Employee(int employeeNumber, double payRate) {
+        if (employeeNumber > MAX_EMPLOYEE_NUMBER) {
+            this.employeeNumber = MAX_EMPLOYEE_NUMBER;
+        } else {
+            this.employeeNumber = employeeNumber;
+        }
+
+        if (payRate > MAX_RATE) {
+            this.payRate = MAX_RATE;
+        } else {
+            this.payRate = payRate;
+        }
+    }
+
+    public double calculateRegularPay(double hoursWorked) {
+        if (hoursWorked > 40) {
+            return 40 * payRate;
+        } else {
+            return hoursWorked * payRate;
+        }
+    }
+
+    public double calculateOvertimePay(double hoursWorked) {
+        if (hoursWorked > 40) {
+            double overtimeHours = hoursWorked - 40;
+            return overtimeHours * (payRate * OVERTIME_MULTIPLIER);
+        } else {
+            return 0.0;
+        }
+    }
+}
